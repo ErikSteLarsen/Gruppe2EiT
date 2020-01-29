8c595409033932575872581848f62aa3f2d11cf6
Eivind Jølsgard <eivinhj@stud.ntnu.no>
2020-01-29 15:16:29 +0100

    add requirement name on requirement, changed name requirements to requirement

---
+++ evaluator/requirement.py
@@ -0,0 +1,35 @@
+from ../truck import Truck
+"""
+This file include functions for checking that different requirements for the truck axle weights are met
+
+TODO: 
+    add more tests
+    Add simple interface to use
+    Improve Requirement class
+    A lot more that i dont see at this moment
+
+"""
+
+
+class Requirement:
+    def __init__(self, errorMessage, requirementName, function):
+        self.function = function
+        self.errorMessage = errorMessage
+	self.requirementName = requirementName
+
+    def testRequirement(self, Truck, Measurement):
+        """
+        Run test for specified requirement object on Truck object.
+        
+        Output: Error message if test fail
+
+        Return: True/false based on pass/fail.
+
+        """
+        if (self.function(Truck, Measurement)):
+            return true
+        else:
+            print(self.ErrorMessage)
+            return false
+
+   
--- evaluator/truckRequirements.py
+++ evaluator/truckRequirements.py
@@ -1,3 +1,13 @@
+import Requirement
+
+truckRequirements = []
+
+def addTruckRequirement(requirementName, errorMessage="No error message added", function):
+	truckRequirements.append(Requirement(errorMessage, requirementName, function))
+
+
+
+
 def hasFrontAxleEnoughWeight(Truck, Measurement):
     """
     This function will check if the front axle has at least 20% of the total weight
@@ -5,3 +15,9 @@ def hasFrontAxleEnoughWeight(Truck, Measurement):
     Return: True if test pass, false if test fail
     """
  
+
+"""
+-----------------------------------------------------------------------------------
+The following code add the functions to a Truck Requirements Object
+"""
+addTruckRequirement(hasFrontAxleEnoughWeight, "FrontAxleWeight20%" ,  "Front axle weight is too low")
