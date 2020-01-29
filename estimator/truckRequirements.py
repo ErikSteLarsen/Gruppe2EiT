c74a6173a3e14fab4257e5ba344a33127eb864d9
Eivind Jølsgard <eivinhj@stud.ntnu.no>
2020-01-29 14:06:42 +0100

    Add requirement class
    Added requirement class and created truck requirement file for functions that check if axle weights are within regulations

---
+++ estimator/truckRequirements.py
@@ -0,0 +1,7 @@
+def hasFrontAxleEnoughWeight(Truck, Measurement):
+    """
+    This function will check if the front axle has at least 20% of the total weight
+
+    Return: True if test pass, false if test fail
+    """
+ 
