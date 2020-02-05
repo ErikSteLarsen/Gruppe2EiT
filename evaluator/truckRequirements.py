89605bc532995820cbd14d629bb97e3842de5c9f
Anders Ajer <ajeranders@gmail.com>
2020-02-05 13:08:20 +0100

    issue 15 part 2

--- evaluator/truckRequirements.py
+++ evaluator/truckRequirements.py
@@ -8,6 +8,12 @@ truckRequirements = []
 def addTruckRequirement(requirementName, function, errorMessage="No error message added"):
 	truckRequirements.append(Requirement(errorMessage, requirementName, function))
 
+
+"""
+    This function will check if the front axle has at least 20% of the total weight
+
+    Return: true if test pass, false if test fail
+    """
 def hasFrontAxleEnoughWeight(Truck, TruckMeasurement):
     axleWeights = TruckMeasurement.getMeasuredAxleWeights()
     sum_weight = 0
@@ -17,16 +23,29 @@ def hasFrontAxleEnoughWeight(Truck, TruckMeasurement):
         return False
     return True
 
+def exceedsTotalWeight(Truck, TruckMeasurement):
+    totalWeight = TruckMeasurement.getTotalWeight()
+    allowedWeight = Truck.getMaxTotalWeight()
+    if totalWeight > allowedWeight:
+        return False
+    return True
+
+def exceedWeightOnOneOfAxels(Truck, TruckMeasurement):
+    axels = Truck.getMaxAxelWeights()
+    axelWeights = []
+    for element in axels:
+        axelWeights.append(element[1])
+    measuredWeights = TruckMeasurement.getMeasuredAxleWeights()
+    for i in range(len(axelWeights)):
+        if axelWeights[i] < measuredWeights[i]:
+            return False
+    return True
 
 
-    """
-    This function will check if the front axle has at least 20% of the total weight
 
-    Return: true if test pass, false if test fail
-    """
 
-"""
 ---------------------------
 The following code add the functions to a Truck Requirements Object
 """
-addTruckRequirement("EnoughWeightOnFrontAxle", "Front axle does not have 20% of total weight", hasFrontAxleEnoughWeight)
+addTruckRequirement("EnoughWeightOnFrontAxle", "Front axle does not have at least 20% of total weight", hasFrontAxleEnoughWeight)
+addTruckRequirement("Totalweight", "The total weight is exceeded!", )
