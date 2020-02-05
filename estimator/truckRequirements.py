20dd7cf896149bfb2c15aeb69a8c69220c5db474
Otto Lote <otto@lote.no>
2020-02-05 09:23:56 +0100

    Remove estimator duplicate folder

--- estimator/truckRequirements.py
+++
@@ -1,7 +0,0 @@
-def hasFrontAxleEnoughWeight(Truck, Measurement):
-    """
-    This function will check if the front axle has at least 20% of the total weight
-
-    Return: True if test pass, false if test fail
-    """
- 
