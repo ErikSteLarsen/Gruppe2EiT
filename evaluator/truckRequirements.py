745cde4b4da656abfb942db94a57d692aac7906a
Eivind Jølsgard <eivinhj@stud.ntnu.no>
2020-02-05 09:14:50 +0100

    fix truck requirement example

--- evaluator/truckRequirements.py
+++ evaluator/truckRequirements.py
@@ -10,12 +10,13 @@ def hasFrontAxleEnoughWeight(Truck, Measurement):
     """
     This function will check if the front axle has at least 20% of the total weight
 
-    Return: True if test pass, false if test fail
+    Return: true if test pass, false if test fail
     """
+	
 
 """
 ---------------------------
 The following code add the functions to a Truck Requirements Object
 """
-addTruckRequirement(hasFrontAxleEnoughWeight, "FrontAxleWeight20%" ,  "Front axle weight is too low")
+addTruckRequirement("EnoughWeightOnFrontAxle", "Front axle does not have 20% of total weight", hasFrontAxleEnoughWeight)
 
