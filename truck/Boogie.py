cbf8678096345bc8b972b56fa34bb6c2678ce772
Eivind Jølsgard <eivinhj@stud.ntnu.no>
2020-02-19 09:00:00 +0100

    Clean Boogie, add Boogies to Trailer

--- truck/Boogie.py
+++ truck/Boogie.py
@@ -16,14 +16,19 @@ class Boogie:
         self.driveShaft = driveShaft
 
 class Boogies:
-
+    """
+    Creates an array of all wheelsets on a Truck or Trailer object
+    INIT: list of Axles with maxWeight and distance to next axle
+    
+    Wheelset can be extracted with getBoogies()
+    """
     def __init__(self, axles):
         self.boogies = [] #List of Boogie objects
         for i in range(1, len(axles)):
             if(axles[i][0] < 1800):
                 if(i >= len(axles)-1): #if last axle
                     self.boogies.append(Boogie([i-1, i], axles[i][0]))
-                elif(axle[i+1][0] < 1800):
+                elif(axles[i+1][0] < 1800):
                     self.boogies.append(Boogie([i-1, i, i+1], min(axles[i-1][0], axles[i][0])))
                     i = i+2
                 else:
