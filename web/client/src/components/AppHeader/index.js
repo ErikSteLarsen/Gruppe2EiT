63b4da7d263433612518317a05d461ce910412f4
Otto Lote <otto@lote.no>
2020-03-18 13:48:38 +0100

    Initial framework restructured in containers and components

---
+++ web/client/src/components/AppHeader/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+const AppHeader = () => {
+  return (
+    <header className="AppHeader">
+      <p>
+        Gruppe2 EiT
+      </p>
+    </header>
+  );
+}
+
+export default AppHeader;
---
+++ web/client/src/containers/WeightMonitorContainer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css'
+
+const WeightMonitorContainer = () => {
+  return (
+    <div className="WeightMonitorContainer">
+      <p>
+        This is where the weight monitor components will appear
+      </p>
+    </div>
+  )
+}
+
+export default WeightMonitorContainer;
