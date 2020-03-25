84d1076a86f746137e2c6e83aa86612609066022
Erik Larsen <erikstelarsen@outlook.com>
2020-03-25 12:57:49 +0100

    designforslag frontend

---
+++ web/client/src/containers/FeedbackContainer/index.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import RegistrationPlate from './';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<FeedbackContainer />);
+  expect(true).to.equal(false); // expect tests to be defined
+});
